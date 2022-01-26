f2cf1aa7c02e8f69ad64748bbe1c9c68353b0273
Ferdinand Hummel <ferdinandhummel@gmx.de>
2022-01-26 16:10:56 +0100

    Changed googletest source dir

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -5,7 +5,6 @@ FetchContent_Declare(
         googletest
         GIT_REPOSITORY https://github.com/google/googletest.git
         GIT_TAG        release-1.11.0
-        SOURCE_DIR     "${PROJECT_SOURCE_DIR}/extern/googletest"
 )
 
 include(GoogleTest)
